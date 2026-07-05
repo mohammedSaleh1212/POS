ae580123464d71e4ebdcd344b3f1d0acf73fce6a
mohammedSaleh1212 <mohammedsaleh1999ms@gmail.com>
2026-07-06 02:52:58 +0300

    add routes to app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import productRoutes from './routes/product.routes';
 import categoriesRoutes from './routes/category.routes';
+import invoiceRoutes from './routes/invoice.routes';
+import movementRoutes from './routes/movement.routes';
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoriesRoutes);
+app.use("/api/invoices", invoiceRoutes);
+app.use("/api/movements", movementRoutes);
 
 
 export default app;
